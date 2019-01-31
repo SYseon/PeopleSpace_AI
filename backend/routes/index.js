e9d0a37568fd32b03bbf51edf6eb6115f02939ab
LDic <vo2qjsld@gmail.com>
2019-01-30 16:12:22 -0800

    no message

---
+++ backend/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var sqlconn = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '1q2w3e4r',
+  database : 'account'
+})
+
+sqlconn.connect();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/start', function(req, res, next)
+{
+  var selectquery = 'SELECT * FROM test';
+  sqlconn.query(selectquery, function(err, rows)
+  {
+    if(err) {console.log(err);}
+    else
+    {
+      console.log(rows);
+    }
+  });
+  res.send("start page");
+});
+router.get('/*', function(req, res)
+{
+  res.send('This is undefined page');
+});
+
+module.exports = router;
