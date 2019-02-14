82b1d4488dfbeb180ef93ae10d3b434fe3f16391
LDic <vo2qjsld@gmail.com>
2019-02-13 17:41:13 -0800

    Implement file upload

--- components/UserBoard.vue
+++ components/UserBoard.vue
@@ -7,7 +7,7 @@
 
       <div class = "cell">
         <label class="bglabel-file"><span class="label-file"></span>
-          <input type = "file" id="file" ref="file" v-on:change="handlerFileUpload()"/>
+          <input type = "file" id="file" ref="file" v-on:change="handleFileUpload()"/> <!------------- ## --------------->
         </label>
         <button class="btn-submit" v-on:click="submitFile()">Submit</button>
         <button class="btn-start" v-on:click="gotoSearch()">Start</button>
@@ -24,6 +24,8 @@
 </template>
 
 <script>
+import axios from 'axios';  //////////////////////////////
+
     export default {
 
       data(){
@@ -52,12 +54,10 @@
               }
             }).then(function(){
               console.log('File submitted');
+              alert("File Submitted!");
             }).catch(function(){
               console.log('File submit Failed');
             });
-
-            alert("File Submitted!");
-
           }
         },
 
@@ -75,13 +75,15 @@
           }
           else{
             /* go to SearchPage */
-            window.location.pathname = '/search'  ////////////////////////// ##
+            window.location.pathname = '/summary'
           }
         },
 
         logout(){
           if(confirm('Do you want to Logout?')){
-            this.$http.post('http://localhost:3000/auth/logout'); ////////////////////////// ##
+            //window.location.pathname = '/'
+            this.$http.post('http://localhost:3000/auth/logout');
+
           } else{
           }
         },
