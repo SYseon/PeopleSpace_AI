c6a40bb4fc61adbad048cfab6baa44ad4b0719de
engyengKim <engyeng9797@gmail.com>
2019-02-15 22:29:09 -0800

    resolving problem - add ReactiveSearch component

---
+++ frontend/src/components/Search2.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <reactive-base
+      app = "entity"
+      url = "https://search-entity-vq4u4jfn4vzumcvld7xprjldzu.us-east-1.es.amazonaws.com/"
+    >
+      <h1>Hello from ReactiveBase</h1>
+      <h3>Search Engine</h3>
+      <div class = "container">
+        <multi-list
+          componentId = "sentiment"
+          dataField = "summary.sentiment"
+          class = "filter"
+          title = "Select sentiment"
+          />
+      </div>
+    </reactive-base>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+
+
+</style>
