5282e2c1f0a84af9646e9c4c41e54ba1f4d999be
LDic <vo2qjsld@gmail.com>
2019-02-16 18:30:55 -0800

    save data to elasticsearch and search data

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,26 +11,21 @@ router.use(bodyParser.urlencoded({extended: false}));
 
 /** ElasticSearch test */
 var client = new elasticsearch.Client({
-  host: 'localhost:9200',
+  host: 'https://search-entity-vq4u4jfn4vzumcvld7xprjldzu.us-east-1.es.amazonaws.com',
   log: 'trace'
 });
 var resultFile = require('../result_test.json');
 
 /** Home Page */
-router.get(['/', '/start', '/welcome'], function(req, res, next) {
+router.get(['/', '/start'], function(req, res, next) {
   if(req.session.bIsLogined) // login already
   {
-    //res.render('welcome', { title: req.session.loginAccount });
     res.redirect('/dashboard');
   }
   else
   {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
-    //res.send('<h1>you should login</h1>');
-    /** pug test */
-    //res.render('index', {title : 'you should login first'});
   }
-  //res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 router.get('/dashboard', function(req, res, next)
 {
@@ -48,12 +43,57 @@ router.get('/summary', function(req, res, next)  // GET summary page
   // Access control
   if(!req.session.bIsLogined)
   {
-    res.redirect('/');
-    return false;
+    //res.redirect('/');
+    //return false;
   }
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
-  //res.render('search');
+
+  // test
+  client.bulk({
+    body: resultFile
+  }, function(err, bulkres, status)
+  {
+    if(err) {console.log('resultFile put error!'), console.log(err);}
+    else
+    {
+      client.search({
+        index: 'entity',
+        //q: `reviewID:${searchData}`
+        body: {
+          size: 0,
+          aggregations: {
+            sentimentSummary: {
+              terms: {
+                field: 'summary.sentiment',
+                order: {_count: 'desc'}  // to get max data as [0]
+              }
+            },
+            emotionSummary: {
+              terms: {
+                field: 'summary.emotion',
+                order: {_count: 'desc'}  // to get max data as [0]
+              }
+            },
+            intentSummary: {
+              terms: {
+                field: 'summary.intent',
+                order: {_count: 'desc'}  // to get max data as [0]
+              }
+            }
+          }
+        }
+      }).then((searchres) => {
+        console.log(searchres.hits.total);
+        // print summary data
+        for(var i in searchres.aggregations)
+        {
+          console.log('i: '+ i + ' / ' + searchres.aggregations[i].buckets[0].key);
+        }
+      });
+    }
+  });
 });
+
 router.post('/summary', function(req, res, next) // POST summary page
 {
   // Access control
@@ -66,13 +106,16 @@ router.post('/summary', function(req, res, next) // POST summary page
   var submittedFile = req.body.submittedFile;
   console.log(submittedFile);
 
-  /** Send submitted file to models */
+  /** Send submitted file to models *
+  var spawn = require("child_process").spawn;
+  var process = spawn('python', ['./testmodel.py', currentPath+'\\'+datafile]);
+  process.stdout.on('data', function(data){
+    console.log('data: '+data);
+  });
+  /
 
   /** Recieve and store result file *
-  client.create({
-    index: 'entity',
-    type: 'review',
-    id: '2',
+  client.bulk({
     body: resultFile
   }, function(err, response, status)
   {
@@ -81,16 +124,24 @@ router.post('/summary', function(req, res, next) // POST summary page
     {
       console.log('==Response==');
       console.log(response);
-      console.log('==Status==');
-      console.log(status);
+      // Search data thru elastic search
+      client.search({
+        index: 'entity',
+        //q: `reviewID:${searchData}`
+        body: {
+          size: 0,
+          aggregations: {
+            summary: {
+              terms: {
+                field: 'summary.emotion'
+              }
+            }
+          }
+        }
+      }).then((response) => {
+        console.log(response.hits.total);
+      });
     }
-  });
-   
-  /** Search data thru elastic search *
-  var searchData = 'mskim'; // change to searchData = req.query.searchData;
-  const response = client.search({
-    index: 'entity',
-    q: `reviewID:${searchData}`
   });
 
   /** Show result data */
@@ -104,16 +155,12 @@ router.get('/search', function(req, res, next)  // GET search page
     return false;
   }
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
-  //res.render('search');
 });
 
 router.post('/single-file', upload.single('file'), function(req, res, next)
 {
   // get new file
   var data = req.file;
-  console.log('req.file: '+data);
-  console.log('name: '+data.filename);
-  console.log('name: '+req.body.name);
   res.send('');
 
   // remove previous file
@@ -125,37 +172,50 @@ router.post('/single-file', upload.single('file'), function(req, res, next)
     {
       for(var i in files)
       {
-        if(files[i] == data.filename) {break;}  // except current file
-        fs.unlink(currentPath+'\\'+files[i], function(err)
+        if(files[i] != data.filename)// except current file
         {
-          if(err) {console.log(err);}
-          else {console.log('succesfully deleted!');}
-        });
+          fs.unlink(currentPath+'\\'+files[i], function(err)
+          {
+            if(err) {console.log(err);}
+            else {console.log('succesfully deleted!');}
+          });
+        }
       }
     }
   });
 });
 
-/** Python test *
+/** Python test */
 router.get('/pythontest', function(req, res)
 {
-  // using spawn instead of exec, prefer a stream over a buffer
-  // to avoid maxBuffer issue
-  var spawn = require("child_process").spawn;
-  var process = spawn('python', ['./compute_input.py']);
-  var data = [100, 10, 100, 1000];
-  var dataString = '';
-  process.stdout.on('data', function(data){
-    dataString += data.toString();
+  var currentPath = path.join(__dirname, '../uploads');
+  fs.readdir(currentPath, function(err, files)
+  {
+    var datafile;
+    if(err) {console.log(err);}
+    else
+    {
+      datafile = files[0];
+    }
+    
+    // send file to model
+    var spawn = require("child_process").spawn;
+    var process = spawn('python', ['./testmodel.py', currentPath+'\\'+datafile]);
+    process.stdout.on('data', function(data){
+      console.log('data: '+data);
+    });
+    process.stdout.on('end', function(){
+      //console.log('Sum of numbers=', dataString);
+    });
+    process.stdin.end();
+    //res.send('');
   });
-  process.stdout.on('end', function(){
-    console.log('Sum of numbers=', dataString);
-  });
-  process.stdin.write(JSON.stringify(data));
-  process.stdin.end();
-  res.send(data);
-  console.log(process);
 });
-*/
+
+// recieve result data from model
+router.post('/pythonpost', function(req, res)
+{
+  console.log(req.body);
+});
 
 module.exports = router;
