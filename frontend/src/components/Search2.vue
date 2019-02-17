50bb21d5161ecdc2647fa7393455ead26338f773
engyengKim <engyeng9797@gmail.com>
2019-02-16 17:50:27 -0800

    add css at Search2.vue

--- frontend/src/components/Search2.vue
+++ frontend/src/components/Search2.vue
@@ -4,34 +4,36 @@
       app = "entity"
       url = "https://search-entity-vq4u4jfn4vzumcvld7xprjldzu.us-east-1.es.amazonaws.com/"
     >
-      <h1>Hello from ReactiveBase</h1>
-      <h3>Search Engine</h3>
-      <div class = "container">
+
+      <div class = "filters-container">
+        <data-search
+          componentId = "SearchKeyword"
+          dataField = "keyword"
+          title = "Search Keyword"
+          class = "filter"
+          />
         <multi-list
           componentId = "Sentiment"
           dataField = "summary.sentiment"
           class = "filter"
-          title = "Select sentiment"
+          title = "Select Sentiment"
           selectAllLabel = "All sentiments"
           />
         <multi-list
           componentId = "Emotion"
           dataField = "summary.emotion"
           class = "filter"
-          title = "Select emotion"
+          title = "Select Emotion"
           selectAllLabel = "All emotions"
           />
         <multi-list
           componentId = "Intent"
           dataField = "summary.intent"
           class = "filter"
-          title = "Select intent"
+          title = "Select Intent"
           selectAllLabel = "All intents"
           />
-        <data-search
-          componentId = "SearchKeyword"
-          dataField = "keyword"
-          />
+        </div>
 
         <reactive-list
           componentId = "SearchResult"
@@ -43,23 +45,66 @@
           :size = "5"
           :react="{ and: ['Sentiment', 'Emotion', 'Intent','SearchKeyword']}"
         >
-          <div slot = "renderData" slot-scope = "{ item }">
-            <div class = "review-header">{{item.productID}}</div>
+          <div slot = "renderData" slot-scope = "{ item }" class = "border-review">
+            <div class = "review-header"><div class="product-title">{{item.productID}}</div></div>
+            <div class = "bar-summary">
+              <span class = "sub-title">Sentiment:</span>
+                <span class = "member">{{item.summary.sentiment}}</span>
+              <span class = "sub-title">Emotion:</span>
+                <span class = "member">{{item.summary.emotion}}</span>
+              <span class = "sub-title">Intent:</span>
+                <span class = "member">{{item.summary.intent}}</span>
+            </div>
             <div class = "review-content">{{item.content}}</div>
             <div class = "review-url">{{item.url}}</div><br>
           </div>
         </reactive-list>
-      </div>
     </reactive-base>
   </div>
 </template>
 
 <script>
+import "./style.css";
+
 
 </script>
 
 
 <style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
 
+.product-title{
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 10px;
+  padding-top: 10px;
+}
+
+.border-review{
+  border-bottom-style : outset;
+}
+
+.bar-summary{
+
+}
+
+.sub-title{
+  font-weight: bolder;
+  color: gray;
+}
+
+.member{
+  margin-right: 4px;
+  color: green;
+}
+
+.review-url{
+  color: blue;
+}
 
 </style>
